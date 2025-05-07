3d3e9b893a930453c0b98dc4d2cac48f55193994
Sugar <ItsAbubakarDev@users.noreply.github.com>
2025-05-07 14:37:44 +0500

    upvote and downVote functionality added to campaign.jsx

--- FluxFund/src/Campaign.jsx
+++ FluxFund/src/Campaign.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Campaign.css";
 
-const Campaign = ({ image, title, description, votes, onVote }) => {
+const Campaign = ({ image, title, description, votes: initialVotes }) => {
+  const [votes, setVotes] = useState(initialVotes);
+
+  const upVote = () => {
+    setVotes(votes + 1);
+  };
+
+  const downVote = () => {
+    setVotes(votes - 1);
+  };
+
   return (
     <div className="campaign-card">
       <img src={image} alt={title} className="campaign-image" />
@@ -10,8 +20,8 @@ const Campaign = ({ image, title, description, votes, onVote }) => {
         <p className="campaign-description">{description}</p>
         <div className="campaign-votes">Votes: {votes}</div>
         <div className="campaign-actions">
-          <button onClick={() => onVote(1)}>⬆ Upvote</button>
-          <button onClick={() => onVote(-1)}>⬇ Downvote</button>
+          <button onClick={upVote}>⬆ Upvote</button>
+          <button onClick={downVote}>⬇ Downvote</button>
         </div>
       </div>
     </div>
