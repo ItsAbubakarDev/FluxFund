7495ffb535fc8d641a8074ba925ffed67d8ff971
Sugar <ItsAbubakarDev@users.noreply.github.com>
2025-05-09 01:44:14 +0500

    Fixed Header.jsx name for deployment

--- FluxFund/src/App.jsx
+++ FluxFund/src/App.jsx
@@ -1,7 +1,7 @@
 // App.js
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./Header";
+import Header from ".Header";
 import Footer from "./Footer";
 import Home from "./HomePage";
 import AllCampaigns from "./AllCampaigns";
